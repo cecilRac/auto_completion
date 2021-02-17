30faaf8725d0f4ffff52f9c6b1d85fc44c590389
cecil-xpo <cecil@frontdoorapp.com>
2021-02-17 10:33:01 +0100

    Refactored -ts. do MR

--- src/core/dictionary.ts
+++ src/core/dictionary.ts
@@ -5,14 +5,14 @@ import Tree from '../classes/Tree' ;
 const dictionary =  new Tree;
 
 // read keywords from <file> into memory
-export const load_dictionary = async (file) => {
+export const load_dictionary = (file): void => {
     let words = fs.readFileSync(file, 'utf8').split('\n')
     words.map(word => dictionary.add(word.toLowerCase()))
 
 }
 
 // get <num> possible sugesstions for <text>
-export const get_suggestions = async (text , num = 4) => {
+export const get_suggestions = async (text , num = 4): Promise <string[]> => {
     dictionary.clear_suggestions();
     const array_suggestions = dictionary.suggest_match(text.toLowerCase(), num);
     return array_suggestions.map(el => el.word)
